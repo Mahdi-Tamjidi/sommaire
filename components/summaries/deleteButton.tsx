f9eb240a7e8eb4a115e008893395a9e28687b8a0
Mahdi <mahditamjidi4@gmail.com>
2025-11-09 12:01:01 +0330

    Strted the summaries page and connected it to the db now it shows the cards for each summary in the sb under the user_id

---
+++ components/summaries/deleteButton.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+
+const DeleteButton = () => {
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className="text-gray-500 bg-gray-50 border border-gray-200 hover:text-rose-600 hover:bg-rose-50"
+    >
+      <Trash2 className="w-4 h-4" />
+    </Button>
+  );
+};
+export default DeleteButton;
